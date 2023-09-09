f25cedab55eece5751695271b630dc0f3b6f6bee
samarpan sarkar <samarpansarkar209@gmail.com>
2023-09-09 21:45:44 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -9,23 +9,23 @@ import {
   Possibility,
   WhatGTP3,
 } from "./containers";
-import {Brand, CTA, Navbar } from "./components";
+import { Brand, CTA, Navbar } from "./components";
 
 function App() {
   return (
     <React.Fragment>
       <div className="App">
         <div className="gradient__bg">
-          <Navbar />
-          <Header />
-        </div>
-        <Brand />
-        <WhatGTP3 />
-        <Features />
-        <Possibility/>
-        <CTA />
-        <Blog />
-        <Footer />
+          <Navbar />  {/* done */}
+          <Header />  {/* done */}
+        </div>        
+        <Brand />     {/* done */}
+        <WhatGTP3 />  {/* done */}
+        <Features />  {/* done */}
+        <Possibility />{/* todo */}  {/*  2:11:48        https://www.youtube.com/watch?v=F627pKNUCVQ&ab_channel=JavaScriptMastery*/}
+        <CTA />       {/* todo */}
+        <Blog />      {/* todo */}
+        <Footer />    {/* todo */}
       </div>
     </React.Fragment>
   );

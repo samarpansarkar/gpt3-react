0ccbcc80a1395721015322b5346909fb5df4f38f
samarpan sarkar <samarpansarkar209@gmail.com>
2023-09-09 21:00:26 +0530

    adding brand (1:38:10)
    https://www.youtube.com/watch?v=F627pKNUCVQ&ab_channel=JavaScriptMastery

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import './brand.css'
+import React from "react";
+import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 const Brand = () => {
   return (
-    <div>
-      <h2>Brand..</h2>
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Brand
+export default Brand;
